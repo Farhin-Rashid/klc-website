6c827d73ff427ccb11e620a58c7eda42fa151753
Farhin-Rashid <123594031+Farhin-Rashid@users.noreply.github.com>
2023-07-08 13:09:07 -0500

    Add links to different pages

--- server.js
+++ server.js
@@ -7,9 +7,35 @@ const port = 3000;
 
 app.set("view engine", "ejs");
 
-app.get("/", function(req, res){
-    console.log("Works");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get('/', function(req, res){
     res.render("home");
+});
+
+app.get("/about", function(req,res){
+    res.render("about")
+});
+
+app.get('/contact', function(req,res){
+    res.render("contact")
+});
+
+app.get('/classes', function(req,res){
+    res.render("classes")
+});
+
+app.get('/faq', function(req,res){
+    res.render("faq")
+});
+
+app.get('/events', function(req,res){
+    res.render("events")
+});
+
+app.get('/resources', function(req,res){
+    res.render("resources")
 });
 
 app.listen(port, function(){
