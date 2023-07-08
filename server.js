869845482b8ed265295300b2b0bda6603bab48fd
Farhin-Rashid <123594031+Farhin-Rashid@users.noreply.github.com>
2023-07-08 11:30:51 -0500

    Add footer, header and homepage

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
 
 const app = express();
 const port = 3000;
 
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res){
-    
+    console.log("Works");
+    res.render("home");
 });
 
 app.listen(port, function(){
