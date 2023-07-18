f3492676e13ddb14f02f08bfc39298e7c0675290
Farhin-Rashid <123594031+Farhin-Rashid@users.noreply.github.com>
2023-07-17 20:55:24 -0500

    Add read more button in home page

---
+++ public/js/home.js
@@ -0,0 +1,7 @@
+$(".readmore").on('click', function(){
+    $(this).parent().toggleClass("showContent");
+
+    var replaceText = $(this).parent().hasClass("showContent") ? "Read Less" : "Read More";
+
+    $(this).text(replaceText);
+});
